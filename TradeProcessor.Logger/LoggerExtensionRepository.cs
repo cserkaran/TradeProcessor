8a6b60f796689796b717adc55670d42245b07532
Karan Sharma <karan@id.com.au>
2018-02-05 17:03:01 +1100

    Adding Extension Provider for Logging.

---
+++ TradeProcessor.Logger/LoggerExtensionRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradeProcessor.Interfaces.Extensibility;
+
+namespace TradeProcessor.Logger
+{
+    internal class LoggerExtensionRepository : IExtensionRepository
+    {
+        public void ProcessImports()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
