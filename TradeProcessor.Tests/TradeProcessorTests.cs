965be82bcece274e4e56eba2d468c7f01c4c7ee1
Karan Sharma <karan@id.com.au>
2018-02-06 17:22:51 +1100

    Adding Unit tests.

---
+++ TradeProcessor.Tests/TradeProcessorTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace TradeProcessor.Tests
+{
+    public class TradeProcessorTests
+    {
+
+        public TradeProcessorTests()
+        {
+
+        }
+
+        [Fact]
+        public void Test_TradeLine_IsMalformed()
+        {
+
+        }
+    }
+}
